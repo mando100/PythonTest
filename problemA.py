a2036311cf4137bfb6b757a10965fc853625f113
mando100 <armandosoto100@gmail.com>
2018-04-30 10:25:54 -0300

    Change the solutions files to set correct the names

--- problemA.py
+++ problemA.py
@@ -1,42 +1,13 @@
-##Problema B
+##Problema A
 
-def annograms(word):
-  result = []
-  words = [w.rstrip() for w in open('WORD.LST')]
-  sameLen = list(filter(lambda x: len(x) == len(word),words))
-  
-  for str1 in sameLen:
-    if(anagramSolution1(str1,word)):
-      result = [str1] + result 
-  print result
-  
-def anagramSolution1(s1,s2):
-    alist = list(s2)
+def thousands_with_commas(i):
+  print '{:,}'.format(i)
+  formatedValue = '{:,}'.format(i)
 
-    pos1 = 0
-    stillOK = True
+  return formatedValue
 
-    while pos1 < len(s1) and stillOK:
-        pos2 = 0
-        found = False
-        while pos2 < len(alist) and not found:
-            if s1[pos1] == alist[pos2]:
-                found = True
-            else:
-                pos2 = pos2 + 1
 
-        if found:
-            alist[pos2] = None
-        else:
-            stillOK = False
-
-        pos1 = pos1 + 1
-
-    return stillOK  
-
-if __name__ == "__main__":
-	print(annograms("train"))
-	print('--')
-	print(annograms('drive'))
-	print('--')
-	print(annograms('python'))
+if __name__ == '__main__':
+assert thousands_with_commas(1234) == '1,234'
+assert thousands_with_commas(123456789) == '123,456,789'
+assert thousands_with_commas(12) == '12'  
--- problemB.py
+++ problemB.py
@@ -1,15 +1,45 @@
-##Problema A
+##Problema B
 
-def thousands_with_commas(i):
-  print '{:,}'.format(i)
-  formatedValue = '{:,}'.format(i)
+def annograms(word):
+  result = []
+  words = [w.rstrip() for w in open('WORD.LST')]
+  sameLen = list(filter(lambda x: len(x) == len(word),words))
+  
+  for str1 in sameLen:
+    if(anagramSolution1(str1,word)):
+      result = [str1] + result 
+  print result
 
-  return formatedValue
+#This function can be found on: http://interactivepython.org/courselib/static/pythonds/AlgorithmAnalysis/AnAnagramDetectionExample.html  
+def anagramSolution1(s1,s2):
+    alist = list(s2)
 
+    pos1 = 0
+    stillOK = True
 
-if __name__ == '__main__':
-assert thousands_with_commas(1234) == '1,234'
-assert thousands_with_commas(123456789) == '123,456,789'
-assert thousands_with_commas(12) == '12'  
+    while pos1 < len(s1) and stillOK:
+        pos2 = 0
+        found = False
+        while pos2 < len(alist) and not found:
+            if s1[pos1] == alist[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+
+        if found:
+            alist[pos2] = None
+        else:
+            stillOK = False
+
+        pos1 = pos1 + 1
+
+    return stillOK  
+
+if __name__ == "__main__":
+	print(annograms("train"))
+	print('--')
+	print(annograms('drive'))
+	print('--')
+	print(annograms('python'))
 
 
